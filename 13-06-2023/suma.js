1c399a75b8ac9e07ed384a5902a9599a3b987964
Taba98674 <133249277+Taba98674@users.noreply.github.com>
2023-06-13 10:28:26 -0500

    Delete 13-06-2023 directory

--- 13-06-2023/suma.js
+++
@@ -1,8 +0,0 @@
-function sumar() {
-let numero1=parseInt (document.getElementById("n1").value)
-let numero2=parseInt (document.getElementById("n2").value)
-let suma=numero1+numero2
-// mostrar el resultado 
-document.getElementById("resultado").textContent="la suma es.."+suma;
-
-}
